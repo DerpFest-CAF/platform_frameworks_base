68482b68afb930b9659d11398e0bea6a3e3457e5
cjh1249131356 <cjh1249131356@gmail.com>
2024-09-07 12:57:10 +0800

    base: Add custom display resolution switch support
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    [nurkeinneid: Update to Android 15 QPR2]
    Co-authored-by: NurKeinNeid <nurkeinneid@derpfest.org>
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -3817,7 +3817,8 @@ public class WindowManagerService extends IWindowManager.Stub
             if (mDisplayReady) {
                 final int forcedDensity = getForcedDisplayDensityForUserLocked(newUserId);
                 final int targetDensity = forcedDensity != 0
-                        ? forcedDensity : displayContent.getInitialDisplayDensity();
+                        ? forcedDensity : WindowManagerServiceExt.getInstance()
+                                .getDensityWithScale(displayContent.getInitialDisplayDensity());
                 displayContent.setForcedDensity(targetDensity, UserHandle.USER_CURRENT);
             }
         }
@@ -6168,7 +6169,8 @@ public class WindowManagerService extends IWindowManager.Stub
         synchronized (mGlobalLock) {
             final DisplayContent displayContent = mRoot.getDisplayContent(displayId);
             if (displayContent != null && displayContent.hasAccess(Binder.getCallingUid())) {
-                return displayContent.getInitialDisplayDensity();
+                return WindowManagerServiceExt.getInstance()
+                        .getDensityWithScale(displayContent.getInitialDisplayDensity());
             }
 
             DisplayInfo info = mDisplayManagerInternal.getDisplayInfo(displayId);
@@ -6244,8 +6246,9 @@ public class WindowManagerService extends IWindowManager.Stub
             synchronized (mGlobalLock) {
                 final DisplayContent displayContent = mRoot.getDisplayContent(displayId);
                 if (displayContent != null) {
-                    displayContent.setForcedDensity(displayContent.getInitialDisplayDensity(),
-                            callingUserId);
+                    displayContent.setForcedDensity(WindowManagerServiceExt.getInstance()
+                            .getDensityWithScale(displayContent.getInitialDisplayDensity()),
+                                    callingUserId);
                 } else {
                     DisplayInfo info = mDisplayManagerInternal.getDisplayInfo(displayId);
                     if (info != null) {
